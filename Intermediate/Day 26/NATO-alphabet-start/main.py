294e61b6dc5ba4313a62fb8fe74ae7a0b27bb391
Daniel <Daniel@MacBook-Pro.attlocal.net>
2021-11-06 15:17:52 -0500

    various changes

--- Intermediate/Day 26/NATO-alphabet-start/main.py
+++ Intermediate/Day 26/NATO-alphabet-start/main.py
@@ -4,7 +4,17 @@ import pandas
 nato_data = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row[0]: row[1] for idx, row in nato_data.iterrows()}
 
+
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-nato_list = [nato_dict[letter] for letter in word]
-print(nato_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        nato_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Please enter only letters in the alphabet.")
+        generate_phonetic()
+    else:
+        print(nato_list)
+
+
+generate_phonetic()
--- Intermediate/Day 29-30/main.py
+++ Intermediate/Day 29-30/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import password_generator
 import pyperclip
+import json
 
 DEFAULT_EMAIL = "daniel.changk@gmail.com"
 
@@ -26,22 +27,62 @@ def save():
     email = email_input.get()
     website = website_input.get()
     password = password_input.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if not website or not password:
         messagebox.showinfo(message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: "
-                                                              f"\nEmail: {email} "
-                                                              f"\nPassword: {password} "
-                                                              f"\nIs it ok to save?")
-        if is_ok:
-            # Write entry field values to data.txt file
-            with open("data.txt", mode="a") as data_file:
-                data_file.write(f"\n{website} | {email} | {password}")
-
+        try:
+            with open("data.json", mode="r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            write_to_json(new_data)
+        else:
+            # Updating and writing old data with new data
+            data.update(new_data)
+            write_to_json(data)
+        finally:
             # Clear website and password entry fields
             website_input.delete(0, END)
             password_input.delete(0, END)
+
+
+def write_to_json(new_data):
+    """
+    Writes new_data dictionary to json data.json file
+    :param new_data: dictionary
+    """
+    with open("data.json", "w") as data_file:
+        # Create new json file and write new_data to it
+        json.dump(new_data, data_file, indent=4)
+
+
+# ---------------------------- SEARCH WEBSITE LOGIN ------------------------------- #
+def find_password():
+    """
+    finds password for website entered as input if exists in data.json file
+    """
+    website = website_input.get()
+    try:
+        with open("data.json", mode="r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(message="No Data File Found")
+    else:
+        if website in data:
+            msg = f"{website}\n" \
+                  f"Email: {data[website]['email']}\n" \
+                  f"Password: {data[website]['password']}"
+        else:
+            msg = f"No details for {website} exists."
+
+        messagebox.showinfo(title=website, message=msg)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -65,18 +106,20 @@ email_label.grid(column=0, row=2)
 password_label.grid(column=0, row=3)
 
 # Entry Widgets
-website_input = Entry(width=36)
+website_input = Entry(width=22)
 website_input.focus()
 email_input = Entry(width=36)
 email_input.insert(END, DEFAULT_EMAIL)
 password_input = Entry(width=22)
-website_input.grid(column=1, row=1, columnspan=2)
+website_input.grid(column=1, row=1)
 email_input.grid(column=1, row=2, columnspan=2)
 password_input.grid(column=1, row=3)
 
 # Buttons
+search_button = Button(text="Search", width=13, command=find_password)
 generate_button = Button(text="Generate Password", command=generate)
 add_button = Button(text="Add", width=37, command=save)
+search_button.grid(column=2, row=1)
 generate_button.grid(column=2, row=3)
 add_button.grid(column=1, row=4, columnspan=2)
 
